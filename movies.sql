106073226e86e32aa1f3c00baed47c434fb534a2
jmona789 <jmona789@gmail.com>
2016-01-27 19:08:42 -0500

    made sql file for movies DB

---
+++ movies.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE movies;
+USE movies;
+CREATE TABLE samuel_l_jackson_movies(
+  name varchar(255),
+  charac varchar(255),
+  year INT
+);
+INSERT INTO samuel_l_jackson_movies(name, charac, year)
+VALUES ("Jackie Brown", "Ordell Robbie", 1997);
+INSERT INTO samuel_l_jackson_movies(name, charac, year)
+VALUES ("Shaft", "John Shaft", 2000);
+INSERT INTO samuel_l_jackson_movies(name, charac, year)
+VALUES ("Snakes on a Plane", "Neville Flynn", 2006);
+INSERT INTO samuel_l_jackson_movies(name, charac, year)
+VALUES ("Star Wars: The Clone Wars", " Mace Windu", 2008);
+INSERT INTO samuel_l_jackson_movies(name, charac, year)
+VALUES ("Captain America: The First Avenger", "Nick Fury", 2011);
+INSERT INTO samuel_l_jackson_movies(name, charac, year)
+VALUES ("Fury", "Foley", 2012);
+INSERT INTO samuel_l_jackson_movies(name, charac, year)
+VALUES ("The Avengers", "Nick Fury", 2012);
+INSERT INTO samuel_l_jackson_movies(name, charac, year)
+VALUES ("Django Unchained", "Stephen", 2012);
+INSERT INTO samuel_l_jackson_movies(name, charac, year)
+VALUES ("Avengers: Age of Ultron", "Nick Fury", 1015);
